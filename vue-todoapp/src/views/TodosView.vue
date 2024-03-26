34c93c72453f3e4707e3b2007da84c75449a7a43
StephanieTayo <stephanie7tayo@gmail.com>
2024-03-26 12:23:13 -0700

    Initial Commit

---
+++ vue-todoapp/src/views/TodosView.vue
@@ -0,0 +1,70 @@
+<script setup>
+
+
+import {ref} from "vue";
+import { uid } from "uid";
+import TodoCreator from "../components/TodoCreator.vue";
+import TodoItem from "../components/TodoItem.vue";
+import { Icon } from "@iconify/vue";
+
+const todoList= ref([]);
+
+const createTodo = (todo) => {
+  todoList.value.push({
+
+   id: uid(),
+   todo,
+   isCompleted: null,
+   isEditing: null,
+
+  });
+};
+
+</script>
+
+<template>
+  <main>
+<h1> Create Your To dos </h1>
+<TodoCreator @create-todo="createTodo"/>
+<ul class="todo-list" v-if="todoList.length > 0"> 
+  <TodoItem v-for=" todo in todoList" :todo="todo"/>
+</ul>
+
+<p v-else class="todos-msg">
+      <Icon icon="noto-v1:sad-but-relieved-face" />
+      <span>You have no todo's to complete! Add one!</span>
+    </p>
+
+  </main>
+</template>
+<style lang="scss">
+
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
+  }
+}
+</style>
